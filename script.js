3ef2c4e3dda6a724d4b5e1a95235260a91355ce4
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-03-27 12:12:03 -0400

    finished most recent changes

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 console.log('Linked Up!')
 
-let box = document.querySelectorAll('.box')
-console.log(box)
-function colorChange(box){
-    console.log('called')
-    box.style.backgroundColor = 'red'
-}
-box.forEach(item => {
-    item.addEventListener('click', () => colorChange(item))
+let boxes = document.querySelectorAll('.box')
+console.log(boxes)
+// function colorChange(box){
+//     console.log('called')
+//     box.style.backgroundColor = 'red'
+// }
+
+let buttons = document.querySelectorAll('button')
+buttons.forEach(button =>{
+    let color = button.innerText
+    button.addEventListener('click', () => {
+        console.log('clicked color button')
+        boxes.forEach(box => box.addEventListener('click', () => box.style.backgroundColor = color))
+    } 
+    )
 })
+
 // box.style.backgroundColor = 'red'
