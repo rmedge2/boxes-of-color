79065dbba6e68f8872caae17a83dce79d6b7a70f
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-03-28 09:45:11 -0400

    We got hovers

--- script.js
+++ script.js
@@ -7,14 +7,31 @@ console.log(boxes)
 //     box.style.backgroundColor = 'red'
 // }
 
-let buttons = document.querySelectorAll('button')
-buttons.forEach(button =>{
-    let color = button.innerText
+let randomMode
+
+boxes.forEach(box => box.addEventListener('click', ()=>{
+    if(randomMode=false){
+        box.style.backgroundColor = box.style.backgroundColor === color ? 'black' : color
+    }
+    else{
+        box.style.backgroundColor = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`
+    }
+}))
+let color = 'white'
+let buttons = document.querySelectorAll('header > button')
+buttons.forEach(button => {
+    // button.style.backgroundColor = color;
     button.addEventListener('click', () => {
-        console.log('clicked color button')
-        boxes.forEach(box => box.addEventListener('click', () => box.style.backgroundColor = color))
-    } 
+        color = button.innerText
+        console.log(`activated ${color} mode`)
+        randomMode = false;
+    }
     )
 })
 
-// box.style.backgroundColor = 'red'
+// Activates the random mode
+let randomButton = document.querySelector('.random > button')
+randomButton.addEventListener('click', () => {
+    console.log(`Now we have entered the random mode`)
+    randomMode = true
+})
